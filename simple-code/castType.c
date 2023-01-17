1c04b7072d8716aa55e1e1c731f465a9f07ae8b8
JobairHossainTamim <likeabletamim@gmail.com>
2023-01-17 15:15:23 +0600

    programming

---
+++ simple-code/castType.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    double x;
+    x = 10.5;
+    n = (int)x;
+    printf("value of n %d ", n);
+    printf("value of n %lf ", x);
+    return 0;
+}
