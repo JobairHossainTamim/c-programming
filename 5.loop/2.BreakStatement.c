f9bbfcadd7f46f7cedb5dd2ba1a776222459f0bf
JobairHossainTamim <likeabletamim@gmail.com>
2023-01-28 15:53:19 +0600

    break Statement

---
+++ 5.loop/2.BreakStatement.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i = 0;
+    while (i <= 100)
+    {
+        printf("%d \n", i);
+        i++;
+
+        if (i > 20)
+        {
+            break;
+        }
+    }
+    return 0;
+}
